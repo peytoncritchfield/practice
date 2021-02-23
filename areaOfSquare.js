ea555ad4b6552f99d3fce021ab300f8b4dfce4ba
peyton critchfield <peyton.developer@gmail.com>
2021-02-23 15:56:23 -0500

    first commit

---
+++ areaOfSquare.js
@@ -0,0 +1,15 @@
+let areaOfSquare = function (x1,x2,y1,y2) {
+    heightOfSquare = y1 - y2;
+    widthOfSquare = x2 - x1;
+    area = heightOfSquare * widthOfSquare;
+    if (area >= 0) {
+        return area;
+    }
+    else {
+        return -area;
+    }
+}
+
+areaOfSquare (2,3,5,7)
+
+
